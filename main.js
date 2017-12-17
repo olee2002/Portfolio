c6d082d8e732e36b77a38a8fe432737b6dcc5e7c
babylee2002 <babylee2002@gmail.com>
2017-12-17 01:08:42 -0500

    Initial portfolio design

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 $(document).ready(function () {
 
-    $('.tlt').textillate({ in: { effect: 'rollIn' } });
-    
+    $('.tlt').textillate({
+        in: {
+            effect: 'fadeInDown',
+            shuffle: 'true',
+            delay: '50',
+            minDisplayTime:1000,
+            initialDelay: 0,
+        },
+        out: {
+            effect: 'fadeInUp',
+            shuffle: 'true',
+            minDisplayTime:1000,
+            initialDelay: 1000,
+        },
+
+        loop: false
+    });
+    // $('.tlt').textillate();
+
+
 
 
 });
+
