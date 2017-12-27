e4b708494e6e0e762717b6b083d22fb5a6a78ea8
babylee2002 <babylee2002@gmail.com>
2017-12-26 22:19:47 -0500

    about me added

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
             effect: 'fadeInDown',
             shuffle: 'true',
             delay: '50',
-            minDisplayTime:1000,
+            minDisplayTime: 1000,
             initialDelay: 0,
         },
         out: {
             effect: 'fadeInUp',
             shuffle: 'true',
-            minDisplayTime:1000,
+            minDisplayTime: 1000,
             initialDelay: 1000,
         },
 
@@ -19,7 +19,33 @@ $(document).ready(function () {
     });
     // $('.tlt').textillate();
 
+    $("img").mouseenter(() => {
+        $(event.target).css({
+            "opacity": "0.25",
+            "transform": "translateY(2px)",
+        })
+    })
+    $("img").mouseleave(() => {
+        $(event.target).css({
+            "opacity": "1",
+            "transform": "translateY(-2px)",
+        })
+    })
 
+    $("img").click(() => {
+        $("#statement")
+            .addClass("animated slideInRight")
+            .html("Olee is a full-stack software engineer with a strong design background as an architect creating physical spaces.")
+            .css({
+                "font-size": "3.5vh",
+                "margin": "5vh",
+                "padding": "5vh"
+            })
+    
+        $("#olee").addClass("animated bounce").css("display", "block")
+    
+    
+    })
 
 
 });
