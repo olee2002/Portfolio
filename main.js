8ff34b6e5f379d9d9c0e69c505c8d57e64e50049
babylee2002 <babylee2002@gmail.com>
2018-02-19 23:42:20 -0500

    nav-bars added

--- main.js
+++ main.js
@@ -4,61 +4,66 @@ $(document).ready(function () {
         in: {
             effect: 'fadeInDown',
             shuffle: 'true',
-            delay: '50',
+            delay: '75',
             minDisplayTime: 500,
             initialDelay: 0,
+            callback: function () {
+                // $('.tlt').textillate('out');
+            }
         },
         out: {
             effect: 'rotateOut',
             shuffle: 'true',
             minDisplayTime: 1000,
             initialDelay: 500,
+            callback: function () {
+                $('.tlt').textillate('stop');
+            }
         },
+        loop: false,
+    })
 
-        loop: true
-    });
-    // $('.tlt').textillate();
-    
     $("img").mouseenter(() => {
         $(event.target).css({
             "opacity": "0.25",
-            "transform": "translateY(2px)",
+            "transform": "translateY(-2px)",
         })
     })
     $("img").mouseleave(() => {
         $(event.target).css({
             "opacity": "1",
-            "transform": "translateY(-2px)",
+            "transform": "translateY(2px)",
         })
     })
-
-   
 
     $(".aboutMe").click(() => {
         $("#statement")
             .addClass("animated fadeIn")
             .html("Olee is a full-stack software developer with a strong design background. In her previous career as an architect, the process of designing skyscrapers trained her to break down complex problems into their constituent parts. No one could build the projects alone, it was always the result of collaborative effort. Due to that, she grew to be an excellent team player.")
             .css({
-                "font-size": "2.75vh",
-                "text-shadow":"0.5px 0.5px rgba(0, 0,0, 0.25)"
+                "font-size": "2.65vh",
+                "text-shadow": "0px 0px rgba(0, 0,0, 0.25)",
+                "font-family": "Montserrat, sans-serif",
+                "top": "225px"
             })
         $("#olee").addClass("animated bounce").css("display", "block")
         $(".allworks").css({
             "display": "none",
         })
     })
-
     $(".works").click(() => {
         $("#statement")
-            .addClass("animated fadeIn")
             .html('')
-            $(".allworks").css({
-                "display": "flex",
-                "z-index": "10"
-            })
-            $("#olee").addClass("animated bounce").css("display", "none")
+        $(".allworks").css({
+            "display": "flex",
+            "z-index": "10"
+        })
+        $("#olee").addClass("animated bounce").css("display", "none")
     })
-    
-  
+
+
+
+
+
 });
 
