16938355a542e8adc6cbd8d0dafaac10eb0e3f4c
babylee2002 <babylee2002@gmail.com>
2018-03-09 09:12:38 -0500

    nav design updated

--- main.js
+++ main.js
@@ -21,17 +21,19 @@ $(document).ready(function () {
                 $('.tlt').textillate('stop')
                 $('.tlt')
                     .html('Click For More Bubbles!')
-                    .addClass("animated fadeInDown")
+                    .addClass("animated zoomOut")
                     .css({
                         "font-size": "35px",
                         "font-family": "Montserrat, sans-serif",
+                        "animation-duration": "7s",
+                        "animation-delay":"0.5s"
                     })
+                
+                    
             },
         },
-
         loop: false,
     })
-
 
     $("a").mouseenter(() => {
         $(event.target).css({
@@ -48,14 +50,15 @@ $(document).ready(function () {
 
     $(".aboutMe").click(() => {
         $("#statement")
-            .addClass("animated fadeIn")
+            // .addClass("animated zoomIn")
             .html("Olee is a full-stack software developer with a strong design background. In her previous career as an architect, the process of designing skyscrapers trained her to break down complex problems into their constituent parts. No one could build the projects alone, it was always the result of collaborative effort. Due to that, she grew to be an excellent team player.")
             .css({
                 "font-size": "2.65vh",
                 "text-shadow": "0px 0px rgba(0, 0,0, 0.25)",
                 "font-family": "Montserrat, sans-serif",
                 "top": "225px",
-                "width":"50vh",
+                "animation-duration": "4s",
+                "animation":"0"
             })
             
         $("#olee").addClass("animated bounce").css("display", "block")
@@ -72,7 +75,14 @@ $(document).ready(function () {
         })
         $("#olee").addClass("animated bounce").css("display", "none")
     })
-
+    $(".design").click(() => {
+        $("#olee").addClass("animated bounce").css("display", "block")
+        $("#statement")
+            .html('This page is coming soon!')
+            $(".allworks").css({
+                "display": "none",
+            })
+    })
 
 
 
